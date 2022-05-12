2c28008ccc87247489d27f1151b6e6f107059132
Vitor772 <vitor989santos@gmail.com>
2022-05-11 23:49:44 -0300

    11/05/

--- dash-shop/src/app/app.module.ts
+++ dash-shop/src/app/app.module.ts
@@ -1,26 +1,16 @@
-import {HttpClientModule } from '@angular/common/http'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { DashShopApiService } from './dash-shop-api.service';
-import { ShowProductsComponent } from './show-products/show-products.component';
-import { AddEditProductsComponent } from './add-edit-products/add-edit-products.component';
 
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ShowProductsComponent,
-    AddEditProductsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
+    BrowserModule
   ],
-  providers: [DashShopApiService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
